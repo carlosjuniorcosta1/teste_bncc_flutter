3cccd21ffb9ac76ceeb162bbe88758b279693623
José Costa <carlosjuniorcosta1@gmail.com>
2023-11-18 15:56:11 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/pro_bncc_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pro_bncc_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Student> fetchStudent() async {
+  final response = await http.get(Uri.parse('http://127.0.0.1:5000/diario/aluno/240'));
+
+  if (response.statusCode == 200) {
+ 
+    return Student.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    
+    throw Exception('Failed to load album');
+  }
+}
+
+class Student {
+  final String name;
+
+const Student({
+  required this.name 
+});
+
+factory Student.fromJson(Map<String, dynamic>json) {
+  return Student(
+    name: json['nome'] as String
+  );
+}
+}
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  late Future<Student> futureStudent;
+
+@override
+void initState() {
+
+    super.initState();
+    futureStudent = fetchStudent();
+  }
+
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Fetch Student Api",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Show students"),          
+        ),
+        body: Center(
+          child: FutureBuilder<Student>(
+            future: futureStudent,
+            builder: ((context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.name);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              } 
+              return const CircularProgressIndicator();
+            }
+          ),
+        ),
+
+      ), 
+
+
+    )
+    );     
+     
+  }
+}
+
+
+
+
